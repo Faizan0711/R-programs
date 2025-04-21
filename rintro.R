bdd72a4ed2ab31796e63568385ced29cb64c2df6
Faizan0711 <72196178+Faizan0711@users.noreply.github.com>
2025-04-21 10:57:20 -0400

    Add files via upload

---
+++ rintro.R
@@ -0,0 +1,32 @@
+name1="Faizan"
+name1
+Name1 = "Ali"
+text = "Excellent"
+paste("ExcelR is",text)## concatinate 
+paste("Students are",text)
+paste("Students are",text,"in class")
+print(" i want to print in r",text)
+### "_" and "." are the only special characters allowed for variable names
+# Data types in R
+# logical (TRUE or T | FALSE or F) 
+x=10.2
+class(x)
+class(3.14)
+#Numeric - num and floats
+#Integer - at the end of the number write L"
+class(-2L)
+# interger division "%/%" | modulo division "%%"
+pi
+??constants
+LETTERS
+letters
+month.name
+month.abb
+### Vectors: 'c', DataFrames
+# Vector of strings
+fruits = c('banana','apple')
+mix = c('faizan',T,45)
+class(mix)
+1:7
+seq(0,20,by=5)
+seq(0,-100,-2)
